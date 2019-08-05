bf07924bd929652b48665fb4f24c320168d75144
Brydon <Brydon@Brydon-Desktop>
2019-08-05 12:59:05 -0400

    Addition of Book Class

---
+++ BookInventory/src/bookinventory/Book.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bookinventory;
+
+public class Book{
+
+    String ISBN;
+    String lastName;
+    String firstName;
+    String title;
+    double numCopies;
+
+}
