71faf4541d26ed07d84c5eb161361b6bd074a5c6
Brydon <Brydon@Brydon-Desktop>
2019-08-05 13:28:41 -0400

    Addition of classes: Admin, Book, genre, searchLocation, and User

---
+++ BookInventory/src/bookinventory/Admin.java
@@ -0,0 +1,11 @@
+package bookinventory;
+
+public class Admin extends User{
+
+    int IDNumber;
+    String password;
+
+    public void addBook(){
+
+    }
+}
---
+++ BookInventory/src/bookinventory/User.java
@@ -0,0 +1,22 @@
+package bookinventory;
+
+import java.io.*;
+import java.util.*;
+
+public class User{
+
+    private ArrayList<Book> cart = new ArrayList<Book>();
+    private int loginID;
+    private String password;
+
+    public void checkout(){
+
+    }
+
+    public void addToCart(){
+    }
+
+    public void removeFromCart(){
+
+    }
+}
