266ce0d5e6ee9b10606411472cb674b5f4c0648c
parsobry <brydon.parsons@gmail.com>
2019-08-06 14:49:37 -0400

    changed code to allow for book objects in array list

--- BookInventory/src/bookinventory/BookInventory.java
+++ BookInventory/src/bookinventory/BookInventory.java
@@ -12,22 +12,34 @@ import java.util.*;
  */
 public class BookInventory{
 
+    public static List<Book> data = new ArrayList<>();
+
     public static void main(String[] args) throws FileNotFoundException, IOException{
-        Search search = new Search();
-        try{
-            search.searchData("Wisdom", SearchLocation.ISBN);
-        }catch(FileNotFoundException ex){
-            System.out.println("Error: " + ex.getMessage());
-        }
+        // Search search = new Search();
+        // search.searchData("Wisdom", SearchLocation.ISBN);
 
         //import text file into array list
-        List<List<String>> data = new ArrayList<>();
         try(BufferedReader br = new BufferedReader(new FileReader("books.csv"))){
             String line;
+            int counter = 0;
             while((line = br.readLine()) != null){
                 String[] values = line.split(",");
-                data.add(Arrays.asList(values));
+                data.add(new Book());
+                data.get(counter).setTitle(values[0]);
+                data.get(counter).setLastName(values[1]);
+                data.get(counter).setFirstName(values[2].trim());
+//                data.get(counter).setGenre(values[3]);
+                data.get(counter).setISBN(values[4]);
+                data.get(counter).setPrice(Double.parseDouble(values[5]));
+                data.get(counter).setNumCopies(Double.parseDouble(values[6]));
+                counter++;
             }
         }
+        System.out.println(data.get(0).getTitle());
+        System.out.println(data.get(0).getLastName());
+        System.out.println(data.get(0).getFirstName());
+        System.out.println(data.get(0).getISBN());
+        System.out.println(data.get(0).getNumCopies());
+
     }
 }
--- BookInventory/src/bookinventory/Search.java
+++ BookInventory/src/bookinventory/Search.java
@@ -11,28 +11,29 @@ public class Search{
     private String searchKey;
     private SearchLocation searchLocation;
 
-    File dataFile = new File("data.txt");
+    public void searchData(String searchKey, SearchLocation searchLocation){
 
-    public void searchData(String searchKey, SearchLocation searchLocation) throws FileNotFoundException{
-        searchKey = this.searchKey;
-        searchLocation = this.searchLocation;
-
-        Scanner input = new Scanner(dataFile);
-        boolean isFound = false;
-
-        while(input.hasNext()){// find way to search by specific location, unsure how we want to handle it
-            String temp = input.next();
-            if(temp.equals("Brydon")){
-                System.out.println(input.nextLine());
-
-                isFound = true;
-                System.out.println("Match found!");
-                break; // this should end the outer while loop search???
-            }
-        }
-        if(isFound == false){
-            System.out.println("Nothing was found!");
-        }
+        System.out.println(BookInventory.data.get(0).get(0));
     }
 
 }
+
+//        searchKey = this.searchKey;
+//        searchLocation = this.searchLocation;
+//
+//        Scanner input = new Scanner(dataFile);
+//        boolean isFound = false;
+//
+//        while(input.hasNext()){// find way to search by specific location, unsure how we want to handle it
+//            String temp = input.next();
+//            if(temp.equals(searchKey)){
+//                System.out.println(input.nextLine());
+//
+//                isFound = true;
+//                System.out.println("Match found!");
+//                break; // this should end the outer while loop search???
+//            }
+//        }
+//        if(isFound == false){
+//            System.out.println("Nothing was found!");
+//        }
