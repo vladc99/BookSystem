237f1e09891ac7b5881719dd57089f33e983ca2f
parsobry <brydon.parsons@gmail.com>
2019-08-06 10:56:00 -0400

    Added a dummy data.txt file just for testing. Added some more code to the admin and search classes.

--- BookInventory/src/bookinventory/Admin.java
+++ BookInventory/src/bookinventory/Admin.java
@@ -5,20 +5,37 @@
  */
 package bookinventory;
 
+import java.io.*;
+import java.util.*;
+
 public class Admin extends User{
 
     int IDNumber;
     String password;
 
-    public void addBook(){
+    File dataFile = new File("data.txt");
 
+    public void addBook(String title, String lastName, String firstName, Genre genre, String ISBN){
+        try(
+                PrintWriter output = new PrintWriter(dataFile);){// 
+            //Code Execution
+            output.println(title + " " + lastName + " " + firstName + " " + genre + " " + ISBN);// Info for new book, correct format???
+
+        }catch(FileNotFoundException ex){
+            System.out.println("Error: " + ex.getMessage());
+        }
     }
 
-    public void deleteBook(){
+    public void deleteBook(){// This should call the search class in someway...
 
     }
 
     public void editBook(){
+        try{
+            Scanner input = new Scanner(dataFile);
+        }catch(FileNotFoundException ex){
+            System.out.println("Error: " + ex.getMessage());// add error data
+        }
 
     }
 
--- BookInventory/src/bookinventory/Search.java
+++ BookInventory/src/bookinventory/Search.java
@@ -3,8 +3,32 @@
  */
 package bookinventory;
 
+import java.util.*;
+import java.io.*;
+
 public class Search{
 
-    String searchKey;
-    SearchLocation searchLocation;
+    private String searchKey;
+    private SearchLocation searchLocation;
+
+    File dataFile = new File("data.txt");
+
+    public void searchData(String searchKey, SearchLocation searchLocation) throws FileNotFoundException{
+        searchKey = this.searchKey;
+        searchLocation = this.searchLocation;
+
+        Scanner input = new Scanner(dataFile);
+        boolean isFound = false;
+        while(input.hasNext()){// find way to search by specific location, unsure how we want to handle it
+            if(input.next().matches(searchKey)){
+                // idk do something that returns the whole line of data
+                isFound = true;
+                System.out.println("Match found!");
+                break; // this should end the outer while loop search???
+            }
+
+        }
+        System.out.println("Nothing was found!");
+    }
+
 }
