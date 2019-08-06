d18346eeffd41f8f29f7f075a6041498a38e1b3b
parsobry <brydon.parsons@gmail.com>
2019-08-06 13:34:24 -0400

    Added database into working directory Added code that imports the books.csv into the arrayList

--- BookInventory/src/bookinventory/BookInventory.java
+++ BookInventory/src/bookinventory/BookInventory.java
@@ -12,17 +12,22 @@ import java.util.*;
  */
 public class BookInventory{
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args){
-        // TODO code application logic here
+    public static void main(String[] args) throws FileNotFoundException, IOException{
         Search search = new Search();
         try{
             search.searchData("Wisdom", SearchLocation.ISBN);
         }catch(FileNotFoundException ex){
             System.out.println("Error: " + ex.getMessage());
         }
-    }
 
+        //import text file into array list
+        List<List<String>> data = new ArrayList<>();
+        try(BufferedReader br = new BufferedReader(new FileReader("books.csv"))){
+            String line;
+            while((line = br.readLine()) != null){
+                String[] values = line.split(",");
+                data.add(Arrays.asList(values));
+            }
+        }
+    }
 }
