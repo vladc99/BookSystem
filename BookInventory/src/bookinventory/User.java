08d629baab1a31d6f8d75366ca85043d5614a6b1
parsobry <brydon.parsons@gmail.com>
2019-08-06 22:30:33 -0400

    added adits code

--- BookInventory/src/bookinventory/User.java
+++ BookInventory/src/bookinventory/User.java
@@ -1,6 +1,3 @@
-/*
- * @author Brydon Parsons
- */
 package bookinventory;
 
 import java.io.*;
@@ -9,17 +6,27 @@ import java.util.*;
 public class User{
 
     private ArrayList<Book> cart = new ArrayList<Book>();
-    private int loginID;
-    private String password;
+    private double totalCost;
 
     public void checkout(){
-
+        for(int i = 0; i < cart.size(); i++){
+            totalCost += cart.get(i).getPrice();
+        }
     }
 
     public void addToCart(){
+        //cart.add(searchBooks().get(selectedBook));
     }
 
     public void removeFromCart(){
+        //card.remove(searchBooks().get(selectedBook));
+    }
 
+    public ArrayList<Book> searchBooks(){
+        Search.searches.clear();
+        // String searchKey = gui get field
+        // String searchType = gui get field
+        // Search.searchData(String searchKey, String searchType);
+        return Search.getSearches();
     }
 }
